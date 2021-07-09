8643227967ba3ee5aac446b03b64a17e371d1c6d
Tushar Balani <tusharbalani@gmail.com>
2021-07-09 16:03:05 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Video from './assets/video/spotlight.mp4'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section>
+        <video autoPlay loop muted src={Video} />
+      </section>
+      <section>
+      <header id="mu-hero">
+		<div className="container">
+			<div className="mu-hero-area">
+				
+				<div className="mu-hero-top">
+					 {/* Start center Logo  */}
+					<div className="mu-logo-area">
+						{/* <!-- text based logo --> */}
+						<a className="mu-logo" href="index.html"><span>Spotlight</span></a>
+						{/* <!-- Image based logo --> */}
+						{/* <!-- <a class="mu-logo" href="index.html"><img src="assets/images/logo.jpg" alt="logo img"></a> --> */}
+					</div>
+					{/* <!-- End center Logo --> */}
+					<div className="mu-hero-top-info">
+						<ul>
+							<li>
+								<i className="fa fa-envelope" aria-hidden="true"></i>
+								<span>SHAASTRA</span>
+							</li>
+							<li>
+								<div className="mu-telephone">
+									<i className="fa fa-phone" aria-hidden="true"></i>
+									<span>IIT MADRAS</span>
+								</div>
+							</li>
+						</ul>
+						
+					</div>
+				</div>
+
+				{/* <!-- Start hero featured area --> */}
+				<div className="mu-hero-featured-area">
+					<div className="mu-hero-featured-content">
+						<h2>Welcome To The Travel Agency </h2>
+						<h1>WE PROVIDE ONLY THE BEST TOURS</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque ut omnis beatae quam quibusdam facere expedita explicabo eaque non sit. Amet vitae exercitationem ad rerum consequuntur numquam magni nemo dolorem, itaque eius perferendis praesentium consequatur. Dolores, nihil, molestiae sunt tenetur aut similique nostrum deleniti amet minima. Esse magnam inventore, ea.</p>
+
+						<a href="#" className="mu-book-now-btn">Book Now!</a>
+						
+						<div className="mu-scrolldown-area">
+							<a href="#mu-about" className="mu-scrolldown" id="mu-scrolldown"><i className="fa fa-chevron-down" aria-hidden="true"></i></a>
+						</div>
+					</div>
+				</div>
+				{/* <!-- End hero featured area --> */}
+
+			</div>
+		</div>
+	</header>
+      </section>
     </div>
   );
 }
